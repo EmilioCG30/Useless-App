129cbb6a4d9f0c20739a84ac24b2aaefcd9a0937
Emilio CG <111447739+EmilioCG30@users.noreply.github.com>
2023-04-18 22:18:47 -0600

    Initial Commit

---
+++ Useless App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Useless App
+//
+//  Created by Emilio Gispert on 18/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Useless App/Useless_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Useless_AppApp.swift
+//  Useless App
+//
+//  Created by Emilio Gispert on 18/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Useless_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
